ee4910fd222df2e57b2a2174dd1d8b4a572c3c90
Buffjeremy <Buffjeremy@gmail.com>
2019-01-12 16:11:17 -0500

    Add files via upload

---
+++ Web-Dev-Projects/include-practice1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1> test stuff </h1>
+    <?php
+
+    include 'include-practice2.php';
+
+    echo " A $color";
+     ?>
+
+  </body>
+</html>
