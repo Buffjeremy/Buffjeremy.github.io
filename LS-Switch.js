e7697ed366e057f09deb77377153cad7f1cdb13b
Jeremy Schantz <Buffjeremy@gmail.com>
2026-09-13 13:50:09 -0400

    Update LS-Switch.js

--- LS-Switch.js
+++ LS-Switch.js
@@ -1,86 +1,207 @@
-// LS SWITCH ATTEMPT
+/*
+ * LOCAL SHUFFLE
+ * Redemption and music download system
+ *
+ * IMPORTANT:
+ * This file is safe to put on GitHub Pages.
+ *
+ * DO NOT put your Supabase service-role key here.
+ *
+ * After we create the Supabase Edge Function, replace
+ * the URL below with your actual Edge Function URL.
+ */
+
+const REDEEM_FUNCTION_URL =
+  "https://YOUR-PROJECT-REF.supabase.co/functions/v1/redeem";
 
 
-function getSong(){
-  var myInput = document.getElementById('myInput').value
+const input = document.getElementById("myInput");
+const button = document.getElementById("myButton");
+const status = document.getElementById("status");
 
-    switch (myInput){
-      case 'VT813':
-        document.write("<a  download = 'Falterboy-Vine Tree-MP3.mp3' href = 'Falterboy-Vine Tree-MP3.mp3'> clicking here will download your song </a> ");
-        break;
-      case 'CMKM813':
-        document.write("<a  download = 'Falterboy-Cure Me Kill Me MP3.mp3' href = 'Falterboy-Cure Me Kill Me MP3.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'PSS813':
-        document.write("<a  download = 'Falterboy-Public Service Slut MP3.mp3' href = 'Falterboy-Public Service Slut MP3.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'BLC813':
-        document.write("<a  download = 'Falterboy-Better Life Coming.mp3' href = 'Falterboy-Better Life Coming.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'CSI813':
-        document.write("<a  download = 'Falterboy-Curbside Improv.mp3' href = 'Falterboy-Curbside Improv.mp3'> clicking here will download your song </a> ");
-      break;
+const downloadArea = document.getElementById("downloadArea");
+const downloadLink = document.getElementById("downloadLink");
 
-      case 'RWB616':
-        document.write("<a  download = 'Rachel-White Brave.mp3' href = 'Rachel-White Brave.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'RWBTS616':
-        document.write("<a  download = 'Rachel-White By The Sea.mp3' href = 'Rachel-White By The Sea.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'RWGKC616':
-        document.write("<a  download = 'Rachel-White Good Kind Of Criminal.mp3' href = 'Rachel-White Good Kind Of Criminal.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'RWS616':
-        document.write("<a  download = 'Rachel-White Superman.mp3' href = 'Rachel-White Superman.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'RWWYS616':
-        document.write("<a  download = 'Rachel-White When You Smile.mp3' href = 'Rachel-White When You Smile.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'MPTITM616':
-        document.write("<a  download = 'Matt Pleasner-This This Is The Music.mp3' href = 'Matt Pleasner This Is The Music.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'MPCMD616':
-        document.write("<a  download = 'Matt Pleasner-Calm Me Down.mp3' href = 'Matt Pleasner Calm Me Down.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'MPDSOP616':
-        document.write("<a  download = 'Matt Pleasner-Don't Speak Of Poetry.mp3' href = 'Matt Pleasner Don't Speak Of Poetry.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'MPSKTW616':
-        document.write("<a  download = 'Matt Pleasner-She Knows The Weather.mp3' href = 'Matt Pleasner-She Knows The Weather.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'MPWW616':
-        document.write("<a  download = 'Matt Pleasner-World Wars.mp3' href = 'Matt Pleasner-Word Wars.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'CBCOA616':
-        document.write("<a  download = 'Cassidy-Bishop Clash Of Arms.mp3' href = 'Cassidy-Bisher Clash Of Arms.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'CBEIW616':
-        document.write("<a  download = 'Cassidy-Bishop Everything I Want.mp3' href = 'Cassidy-Bisher Everything I Want.mp3'> clicking here will download your song </a> ");
-      break;
-      // Cassidy Bishop Soldier
-      case 'CBS616':
-        document.write("<a  download = 'Cassidy-Bishop Soldier.mp3' href = 'Cassidy-Bisher Soldier.mp3'> clicking here will download your song </a> ");
-      break;
-      // Cassity Bishop Sometimes has the T in the code
-      case 'CBST616':
-        document.write("<a  download = 'Cassidy-Bishop Sometimes.mp3' href = 'Cassidy-Bisher Sometimes.mp3'> clicking here will download your song </a> ");
-      break;
-      case 'CBUTS616':
-        document.write("<a  download = 'Cassidy-Bishop Underneath The Stars.mp3' href = 'Cassidy-Bisher Underneath The Stars.mp3'> clicking here will download your song </a> ");
-      break;
-        case 'NJTAJW616':
-        document.write("<a  download = 'Nicholas-Thomasma Johnny Appleweed.mp3' href = '01 Johnny Appleweed.mp3'> clicking here will download your song </a> ");
-      break;
-        case 'NJTBL616':
-        document.write("<a  download = 'Nicholas-Thomasma Bus Life.mp3' href = '02 Bus Life.mp3'> clicking here will download your song </a> ");
-      break;
-        case 'NJTE616':
-        document.write("<a  download = 'Nicholas-Thomasma 03 Eighteen.mp3' href = '03 Eighteen.mp3'> clicking here will download your song </a> ");
-      break;
-        case 'NJTKOS616':
-        document.write("<a  download = 'Nicholas-Thomasma 10 Kindness Of Strangers.mp3' href = '10 Kindness Of Strangers.mp3'> clicking here will download your song </a> ");
-      break;
-    default:
-      alert('please enter a valid code');
+const songTitle = document.getElementById("songTitle");
+const artistName = document.getElementById("artistName");
+
+
+function showStatus(message, type = "") {
+
+  status.textContent = message;
+
+  status.className = "status";
+
+  if (type) {
+    status.classList.add(type);
+  }
+
 }
+
+
+function hideDownload() {
+
+  downloadArea.hidden = true;
+
+  downloadLink.removeAttribute("href");
+
+}
+
+
+async function redeemSong() {
+
+  const token = input.value.trim();
+
+  hideDownload();
+
+  if (!token) {
+
+    showStatus(
+      "Please enter your redemption code.",
+      "error"
+    );
+
+    return;
+  }
+
+
+  button.disabled = true;
+
+  showStatus(
+    "Checking your redemption code..."
+  );
+
+
+  try {
+
+    const response = await fetch(
+      REDEEM_FUNCTION_URL,
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+          token: token
+        })
+      }
+    );
+
+
+    const data = await response.json();
+
+
+    if (!response.ok) {
+
+      throw new Error(
+        data.error ||
+        "We couldn't redeem that code."
+      );
+
     }
+
+
+    if (!data.signed_url) {
+
+      throw new Error(
+        "The song was found, but no download link was created."
+      );
+
+    }
+
+
+    songTitle.textContent =
+      data.song_title || "Your song is ready!";
+
+
+    if (data.artist) {
+
+      artistName.textContent =
+        "Artist: " + data.artist;
+
+    } else {
+
+      artistName.textContent = "";
+
+    }
+
+
+    downloadLink.href = data.signed_url;
+
+
+    if (data.filename) {
+
+      downloadLink.download =
+        data.filename;
+
+    }
+
+
+    downloadArea.hidden = false;
+
+
+    showStatus(
+      "Your song is ready!",
+      "success"
+    );
+
+  }
+
+
+  catch (error) {
+
+    console.error(
+      "Local Shuffle redemption error:",
+      error
+    );
+
+
+    showStatus(
+      error.message ||
+      "Something went wrong. Please try again.",
+      "error"
+    );
+
+  }
+
+
+  finally {
+
+    button.disabled = false;
+
+  }
+
+}
+
+
+/*
+ * Clicking the button redeems the code.
+ */
+
+button.addEventListener(
+  "click",
+  redeemSong
+);
+
+
+/*
+ * Pressing Enter in the code box
+ * does the same thing.
+ */
+
+input.addEventListener(
+  "keydown",
+  function(event) {
+
+    if (event.key === "Enter") {
+
+      event.preventDefault();
+
+      redeemSong();
+
+    }
+
+  }
+);
